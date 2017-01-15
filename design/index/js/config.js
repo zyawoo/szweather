ea966389fdc1daa4d97470baa22d22efef475bad
zyawoo <zyawoo@kwandi.com>
2017-01-15 20:59:24 +0800

    1

--- design/index/js/config.js
+++
@@ -1,8 +0,0 @@
-//气象数据JSON URL
-//var DATA_PATH = 'http://www.szmb.gov.cn/data_center/Data/';
-//var DATA_ROOT = 'http://121.15.170.106/data_center/';
-var DATA_PATH = '/data_cache/' ;
-//var DATA_ROOT = '/data_center/';
-//var DATA_PATH = 'http://203.175.156.109/data_center/Data/';
-//var DATA_PATH = 'http://121.15.170.98/data_center/Data/';
-var DATA_ROOT = 'http://www.szmb.gov.cn/data_center/' ;
